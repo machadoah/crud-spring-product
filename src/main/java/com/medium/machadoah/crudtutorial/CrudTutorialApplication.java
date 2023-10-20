48428a2a248598e0f73e5e90cc1223fdba54fc3c
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-10-20 03:03:03 -0300

    feat: initialize Spring Boot project.

---
+++ src/main/java/com/medium/machadoah/crudtutorial/CrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.medium.machadoah.crudtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTutorialApplication.class, args);
+    }
+
+}
