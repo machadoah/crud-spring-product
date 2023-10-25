f7f3dff30db32e82d8d819518e91efcdb1520ec0
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-10-25 11:09:32 -0300

    Implementing User and UserRole

---
+++ src/main/java/com/medium/machadoah/crudtutorial/domain/user/UserRole.java
@@ -0,0 +1,17 @@
+package com.medium.machadoah.crudtutorial.domain.user;
+
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+     UserRole(String role){
+         this.role = role;
+     }
+
+     public String getRole(){
+         return role;
+     }
+}
